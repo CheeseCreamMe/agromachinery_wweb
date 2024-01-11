9aa31903e615c7abcdbe98b69507d984b5ad3cee
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-01-11 09:37:33 -0600

    home view

--- app/view/page/pages/homeViewPage.php
+++ app/view/page/pages/homeViewPage.php
@@ -1,1 +1,9 @@
-hhomr<br>
+<title> Agromachinery || Inicio </title >
+<section class = "banner" >
+    <div class = "content-banner" >
+        <p class = "slogan" >
+            " Fortaleciendo tu Produccion "
+        </p>
+        <button class = "btn-more"> ver mas... </button>
+    </div>
+</section>
--- core/config/url.php
+++ core/config/url.php
@@ -3,7 +3,7 @@
 const MODEL ="./app/model/";
 const VIEW ="./app/view/page/pages/";
 const CONTROLLER ="./app/controller/";
-const IMAGE ="";
+const IMAGE ="./public/imageS/";
 const CSS ="./public/css/";
 const JS ="";
 const  TEMPLATE = "./app/view/templates/";
