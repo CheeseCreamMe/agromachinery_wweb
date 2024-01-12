1e379995a142dec7425c9806f4f19dde6860bc9a
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-01-12 13:25:50 -0600

    responsive desing

--- app/view/templates/footerPlantilla.php
+++ app/view/templates/footerPlantilla.php
@@ -1,65 +1,67 @@
 <footer class="footer">
-            <div class="container container-footer">
-                <div class="menu-footer">
-                    <div class="contact-info">
-                        <p class="title-footer">Información de Contacto</p>
-                        <ul>
-                            <li>
-                            <?php echo DIRECCION; ?>
-                            </li>
-                            <li><?php echo SUPPORT_NUMBER;; ?></li>
-                            <li><?php echo E_MAIL; ?></li>
-                        </ul>
-                        <div class="social-icons">
-                            <span class="facebook">
-                            <img src="<?php echo IMAGE; ?>facebook.png" alt="otra vaina" style="width:32px">
-                            </span>
-                            <span class="twitter">
-                            <img src="<?php echo IMAGE; ?>twitter.png" alt="otra vaina" style="width:32px">
-                            </span>
-                            <span class="instagram">
-                            <img src="<?php echo IMAGE; ?>instagram.png" alt="otra vaina" style="width:32px">
-                            </span>
-                            <span class="whatsapp">
-                            <img src="<?php echo IMAGE; ?>whatsapp.png" alt="otra vaina" style="width:32px">
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="information">
-                        <p class="title-footer">Información</p>
-                        <ul>
-                            <li><a href="#">Acerca de Nosotros</a></li>
-                            <li><a href="#">Información Delivery</a></li>
-                            <li><a href="#">Politicas de Privacidad</a></li>
-                            <li><a href="#">Términos y condiciones</a></li>
-                            <li><a href="#">Contactános</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="my-account">
-                        <p class="title-footer">Mi cuenta</p>
-
-                        <ul>
-                            <li><a href="#">Mi cuenta</a></li>
-                            <li><a href="#">Historial de ordenes</a></li>
-                            <li><a href="#">Lista de deseos</a></li>
-                            <li><a href="#">Boletín</a></li>
-                            <li><a href="#">Reembolsos</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="newsletter">
-                        <p class="title-footer">Boletín informativo</p>
-                    </div>
-                </div>
-
-                <div class="copyright">
-                    <p>
-                        Desarrollado por CesarDev  &copy; 2022
-                    </p>
-
-                    <img src="<?php echo IMAGE; ?>logo.jpg" alt="Agromachinery logo" />
+    <div class="container container-footer">
+        <div class="menu-footer">
+            <p class="title-footer">Información de Contacto</p>
+            <div class="contact-info">
+                
+                <ul>
+                    <li>
+                    <A><?php echo DIRECCION; ?></A>
+                    </li>
+                    <li>
+                        <a ><?php echo SUPPORT_NUMBER; ?></a>
+                    </li>
+                    <li>
+                        <a ><?php echo E_MAIL; ?></a>
+                    </li>
+                </ul>
+                <div class="social-icons">
+                    <span class="facebook">
+                        <img class="icon-image"src="<?php echo IMAGE; ?>facebook.png" alt="otra vaina" >
+                    </span>
+                    <span class="twitter">
+                        <img class="icon-image"src="<?php echo IMAGE; ?>twitter.png" alt="otra vaina" >
+                    </span>
+                    <span class="instagram">
+                        <img src="<?php echo IMAGE; ?>instagram.png" alt="otra vaina" >
+                    </span>
+                    <span class="whatsapp">
+                        <img class="icon-image"src="<?php echo IMAGE; ?>whatsapp.png" alt="otra vaina">
+                    </span>
                 </div>
             </div>
-        </footer>
+
+            <div class="information">
+                <p class="title-footer">Información</p>
+                <ul>
+                    <li><a href="#">Acerca de Nosotros</a></li>
+                    <li><a href="#">Información Delivery</a></li>
+                    <li><a href="#">Politicas de Privacidad</a></li>
+                    <li><a href="#">Términos y condiciones</a></li>
+                    <li><a href="#">Contactános</a></li>
+                </ul>
+            </div>
+
+            <div class="my-account">
+                <p class="title-footer">Mi cuenta</p>
+
+                <ul>
+                    <li><a href="#">Mi cuenta</a></li>
+                    <li><a href="#">Historial de ordenes</a></li>
+                    <li><a href="#">Lista de deseos</a></li>
+                    <li><a href="#">Boletín</a></li>
+                    <li><a href="#">Reembolsos</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="copyright">
+            <p>
+                Desarrollado por CesarDev &copy; 2022
+            </p>
+
+            <img src="<?php echo IMAGE; ?>logo.jpg" alt="Agromachinery logo" />
+        </div>
+    </div>
+</footer>
