7236a52b22901a1623b4965c68dc5c8558b635d0
Chesse <jcj23727@gmail.com>
2024-01-24 09:44:43 -0600

    othe changes

--- app/view/templates/footerPlantilla.php
+++ app/view/templates/footerPlantilla.php
@@ -1,43 +1,50 @@
-
- <!-- Footer -->
- <footer class="    py-5">
- <!-- Container -->
- <div class="container">
- <!-- Row -->
- <div class="row">
- <!-- Col -->
- <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
- <!-- Heading -->
- <h5 class="text-uppercase mb-4">About</h5>
- <!-- Text -->
- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
- </div>
- <!-- Col -->
- <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
- <!-- Heading -->
- <h5 class="text-uppercase mb-4">Contact</h5>
- <!-- Text -->
- <p>123 Main St, Anytown, USA</p>
- <p>Phone: (123) 456-7890</p>
- <p>Email: info@example.com</p>
- </div>
- <!-- Col -->
- <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
- <!-- Heading -->
- <h5 class="text-uppercase mb-4">Follow Us</h5>
- <!-- Social Media Links -->
- <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
- <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
- <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
- <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
- </div>
- <!-- Col -->
- </div>
- <!-- Row -->
- </div>
- <!-- Container -->
- </footer>
- <!-- Footer -->
- <!-- Bootstrap JS -->
- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybBud7TlRbs/ic4AwGcFZOxg5DpPt8EgeUIgIwzjWfXQKWA3" crossorigin="anonymous"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+<!-- Footer -->
+<footer class="    py-5">
+    <!-- Container -->
+    <div class="container">
+        <div style=" min-height:10px;" class="row"> </div>
+        <!-- Row -->
+        <div class="row">
+            <!-- Col -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <!-- Heading -->
+                <h5 class="text-uppercase mb-4">About</h5>
+                <!-- Text -->
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+            </div>
+            <!-- Col -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <!-- Heading -->
+                <h5 class="text-uppercase mb-4">Contact</h5>
+                <!-- Text -->
+                <p>123 Main St, Anytown, USA</p>
+                <p>Phone: (123) 456-7890</p>
+                <p>Email: info@example.com</p>
+            </div>
+            <!-- Col -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <!-- Heading -->
+                <h5 class="text-uppercase mb-4">Follow Us</h5>
+                <!-- Social Media Links -->
+                <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-instagram"></i></a>
+                <a class="btn btn-outline-primary btn-floating m-1" href="#!" role="button"><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+            <!-- Col -->
+        </div>
+        <!-- Row -->
+    </div>
+    <!-- Container -->
+</footer>
+<!-- Footer -->
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz4fnFO9gybBud7TlRbs/ic4AwGcFZOxg5DpPt8EgeUIgIwzjWfXQKWA3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+    integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/"
+    crossorigin="anonymous"></script>
--- app/view/templates/headerPlantilla.php
+++ app/view/templates/headerPlantilla.php
@@ -1,4 +1,9 @@
 <html data-bs-theme="dark">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script
+      src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <header>
