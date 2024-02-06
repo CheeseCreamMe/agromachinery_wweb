f0718d5fa61478e4f1f29462b811d85bac5b8037
Chesse <jcj23727@gmail.com>
2024-02-06 14:21:18 -0600

    create funtion to add, read and delete "productos", in model, and cahnge other things in products model

--- app/core/model/productosModel.php
+++ app/core/model/productosModel.php
@@ -10,6 +10,17 @@ require_once "./app/connection/connecction.php";
 
 class productosModelo extends connection {
 
+    protected function buscarProducto($id)
+    {
+        $productoId = self::limpiarCadena($id);
+        $consulta = "SELECT p.id, p.nombre, p.precio, p.descripcion, p.precio_descuento, p.imagen, p.inventario, c.nombre AS categoria, m.nombre AS marca
+        FROM producto p
+        INNER JOIN categoria c ON p.categoria_id = c.id
+        INNER JOIN marca m ON p.marca_id = m.id
+        WHERE p.id = ".$productoId.";";
+        $producto = self::ejecutarConsultaSimple($consulta);
+        return $producto;
+    }
     protected function agregarProducto($producto)
     {}
 
@@ -31,10 +42,41 @@ class productosModelo extends connection {
         $tabla = self::ejecutarConsultaSimple($consulta);
         return $tabla;
     }
-    protected function obtenerVeterinariaTabla(){}
-    protected function obtenerFiltradoMarcaTabla(){}
+    protected function obtenerVeterinariaTabla(){
+        $consulta = "SELECT p.id, p.nombre, p.precio, p.descripcion, p.precio_descuento, p.imagen, p.inventario, c.nombre AS categoria, m.nombre AS marca
+        FROM producto p
+        INNER JOIN categoria c ON p.categoria_id = c.id
+        INNER JOIN marca m ON p.marca_id = m.id
+        WHERE c.nombre = 'Veterinaria';";
+        $tabla = self::ejecutarConsultaSimple($consulta);
+        return $tabla;
+    }
+    protected function obtenerFiltradoMarcaTabla($marca){
+        $marcaId = self::limpiarCadena($marca);
+        $consulta = "SELECT p.id, p.nombre, p.precio, p.descripcion, p.precio_descuento, p.imagen, p.inventario, c.nombre AS categoria, m.nombre AS marca
+        FROM producto p
+        INNER JOIN categoria c ON p.categoria_id = c.id
+        INNER JOIN marca m ON p.marca_id = m.id
+        WHERE m.id = ".$marcaId.";";
+        $tabla = self::ejecutarConsultaSimple($consulta);
+        return $tabla;
+    }
 
-    protected function eliminarProductoId($id){}
+    protected function eliminarProductoId($id){
+        $productoId = self::limpiarCadena($id);
+        $consulta = "DELETE p
+        FROM producto p
+        WHERE p.id = ".$productoId.";";
+        $respuesta = self::ejecutarConsultaSimple($consulta);
+        return $respuesta;
+    }
 
-    protected function editarProducto($id){}
+    protected function editarProducto($id, $precio, $precio_descuento, $descripcion, $imagen_ruta,$nombre , $inventario){
+        $productoId = self::limpiarCadena($id);
+        $new_precio_descuento = $precio;
+        $new_nombre = self::limpiarCadena($nombre);
+        $new_inventario = self::limpiarCadena($inventario);
+        $new_imagen_ruta = $imagen_ruta;
+        $new_descripcion = self::limpiarCadena($descripcion);
+    }
 }
