9dc985fcdb08f1f7fd164f0142ddb9efaed2869f
Chesse <jcj23727@gmail.com>
2024-02-09 12:16:13 -0600

    Updated code to use the latest version of DataTables library (v1.13.7) and fixed a typo in the JavaScript code.

--- app/core/view/template/admin/header.php
+++ app/core/view/template/admin/header.php
@@ -17,9 +17,8 @@
     <!--sweetalert-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!--dataTable-->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
-
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 </head>
 
 <body>
