1725a0ffff042cbf6e9bd241655bb3ed81286a51
Chesse <jcj23727@gmail.com>
2024-02-29 11:22:14 -0600

    Typo changes

--- app/core/view/pages/public/productsViewPage.php
+++ app/core/view/pages/public/productsViewPage.php
@@ -11,7 +11,7 @@
 	<div class="row p-2">
 		<div class="col"></div>
 		<div class="colmd-5">
-			<h3>Fitrar por marcas:</h3>
+			<h3>Filtrar por marcas:</h3>
 		</div>
 		<div class="col"></div>
 	</div>
@@ -19,7 +19,7 @@
 	<div class="row p-2">
 		<div class="col"></div>
 		<div class="colmd-5">
-			<!--select de categorias en la parte superior-->
+			<!--select de categorías en la parte superior-->
 			<select class="form-select" id="mySelect">
 			</select>
 		</div>
@@ -27,17 +27,16 @@
 	</div>
 
 	<div class="row p-2">
-		<!-- inicio de la seccion de productos-->
+		<!-- inicio de la sección de productos-->
 		<section class="contenedor productos ">
 			<!--contenido generado dinamicamnete-->
 		</section>
-		<!--fin de laseccion de productos-->
+		<!--fin de la sección de productos-->
 	</div>
 	<!--js necesario--->
 	<script>
 		let serverUri = "<?php echo URI; ?>";
-		console.log(serverUri);
-		let categoria = "<?php echo $textovar; ?>";
+		let categoria = "<?php echo $textovar; ?>";x
 	</script>
 	<script src="./api/marcas/read.js"></script>
 	<script src="./api/productos/read.js"></script>
